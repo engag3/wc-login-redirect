cb4a21cfdf161b6fd03e4e1ba9f613be5cdeebf3
ENGAG3 Media <media@engag3.us>
2016-06-07 15:00:08 -0700

    First Commit (v1)

---
+++ wc-login-redirect.php
@@ -0,0 +1,63 @@
+<?php
+/*!
+  Plugin Name: E3 WooCommerce Login Rediract
+  Plugin URI: https://github.com/engag3/wc-wholesale-products
+  Description: Redirect to a custom page after logging in based on the user role.
+  Version: 1.0.0
+  Author: ENGAG3 Media
+  Author URI: https://www.engag3.media
+  GitHub Plugin URI: https://github.com/engag3/wc-login-redirect
+  GitHub Branch: master
+
+
+  Copyright 2016  ENGAG3 (email : media@engag3.us)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Redirect users to custom URL based on their role after login
+ *
+ * @param string $redirect
+ * @param object $user
+ * @return string
+ */
+function wc_custom_user_redirect( $redirect, $user ) {
+	// Get the first of all the roles assigned to the user
+	$role = $user->roles[0];
+	$dashboard = admin_url();
+	$myaccount = get_permalink( wc_get_page_id( 'myaccount' ) );
+	if( $role == 'administrator' ) {
+		//Redirect administrators to the dashboard
+		$redirect = $dashboard;
+	} elseif ( $role == 'shop-manager' ) {
+		//Redirect shop managers to the dashboard
+		$redirect = $dashboard;
+	} elseif ( $role == 'editor' ) {
+		//Redirect editors to the dashboard
+		$redirect = $dashboard;
+	} elseif ( $role == 'author' ) {
+		//Redirect authors to the dashboard
+		$redirect = $dashboard;
+	} elseif ( $role == 'customer' || $role == 'subscriber' || $role == 'wholesale' ) {
+		//Redirect customers and subscribers to the "My Account" page
+		$redirect = $myaccount;
+	} else {
+		//Redirect any other role to the previous visited page or, if not available, to the home
+		$redirect = wp_get_referer() ? wp_get_referer() : home_url();
+	}
+	return $redirect;
+}
+add_filter( 'woocommerce_login_redirect', 'wc_custom_user_redirect', 10, 2 );
